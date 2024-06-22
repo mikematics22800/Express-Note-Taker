1385e6026b149e44b3c084d3cd3bec8a2a118206
Michael Medina <michaeljmedina22800@gmail.com>
2024-06-21 23:25:39 -0400

    serving notes.html to /notes, index.html to all other routes, and sending notes in response to get requests to /api/notes

--- server.js
+++ server.js
@@ -11,3 +11,22 @@ const app = express(); // Create an Express.js server
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use(express.static('public')); // Middleware to serve static files from '/public'
+
+// Serve notes.html to /notes
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/notes.html')
+));
+
+// Send notes in response to get requests to /api/notes
+app.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, 'notes.json'), (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+})
+
+// Serve index.html to all other routes
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/index.html')
+));
